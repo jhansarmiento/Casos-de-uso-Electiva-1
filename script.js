a14900a54e6193c5b78faf78e3a710fee7705c3e
jhansarmiento <jhancsn1@gmail.com>
2025-03-16 19:52:38 -0500

    Navbar responsive y separacion de css

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   const menuToggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("menu");
+  const menuLinks = document.querySelectorAll(".menu a");
 
   menuToggle.addEventListener("click", function () {
     menu.classList.toggle("active");
   });
 
-
+  // Cierra el menú cuando se hace clic en un enlace
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      menu.classList.remove("active");
+    });
+  });
 });
