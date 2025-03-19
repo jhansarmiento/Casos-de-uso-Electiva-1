e34f9a6956224afc35e350f2ed5ec4cabcfe37f4
AngelEduar000 <aavendano1@udi.edu.co>
2025-03-19 18:41:38 -0500

    Footer y forms cambios

--- script.js
+++ script.js
@@ -39,3 +39,34 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
+
+// Función para abrir el modal y mostrar la imagen correspondiente
+function openModal(type) {
+  const modal = document.getElementById('myModal');
+  const modalImage = document.getElementById('modalImage');
+
+  // Cambia la imagen según el botón presionado
+  if (type === 'nequi') {
+      modalImage.src = 'https://tecnotiendas.com.co/wp-content/uploads/2021/05/Paga-con-Neki.jpg'; // Ruta de la imagen de Nequi
+  } else if (type === 'paypal') {
+      modalImage.src = 'https://www.paypalobjects.com/marketing/web23/es/consumer/pay-with-qr-code/spanish/graphic-split-section-01-ui_size-all.png'; // Ruta de la imagen de PayPal
+  } else if (type === 'bancolombia') {
+      modalImage.src = 'https://febimbo.com/imagenes/comunicaciones/qrall.jpg'; // Ruta de la imagen de Bancolombia
+  }
+
+  modal.style.display = 'block'; // Muestra el modal
+}
+
+// Función para cerrar el modal
+function closeModal() {
+  const modal = document.getElementById('myModal');
+  modal.style.display = 'none'; // Oculta el modal
+}
+
+// Cierra el modal si se hace clic fuera de la imagen
+window.onclick = function(event) {
+  const modal = document.getElementById('myModal');
+  if (event.target === modal) {
+      modal.style.display = 'none';
+  }
+}
