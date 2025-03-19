a59c81186f7eb153fcec8b666df444378c1f215c
jhansarmiento <jhancsn1@gmail.com>
2025-03-19 00:44:56 -0500

    Avances con la galeria

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ document.addEventListener("DOMContentLoaded", function () {
       menu.classList.remove("active");
     });
   });
+
+  const categoryButtons = document.querySelectorAll(".category-button");
+  const galleryItems = document.querySelectorAll(".gallery-item");
+
+  function showCategory(category) {
+      galleryItems.forEach(item => {
+          item.style.display = item.classList.contains(category) ? "block" : "none";
+      });
+  }
+
+  // Mostrar inicialmente solo los perros
+  showCategory("dog");
+
+  categoryButtons.forEach(button => {
+      button.addEventListener("click", function () {
+          let category;
+          const text = this.textContent.toLowerCase();
+          if (text === "perros") {
+              category = "dog";
+          } else if (text === "gatos") {
+              category = "cat";
+          }
+          showCategory(category);
+      });
+  });
 });
